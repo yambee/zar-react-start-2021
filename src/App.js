72c6fb168f0455db6bc39c885910d3f34e9d3df6
Elias <elias@elias.com>
2021-01-28 21:46:12 +0300

    Homework-2, ver.1

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
 		<div className="App">
 			<header className="App-header">
 				<img src={logo} className="App-logo" alt="logo" />
-				<p>Hello web!</p>
+				<p>Плейсхолдер для сдачи ДЗ!</p>
 			</header>
 		</div>
 	);
